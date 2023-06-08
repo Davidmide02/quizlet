e9d4eea692b5dafd1214843a139d25bf7d825333
Davidmide Adenusi <Davidmide07@gmail.com>
2023-06-08 01:08:39 +0100

    ongoing

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,12 +1,11 @@
-import { Children } from "react"
+import { Children } from "react";
 
-
-const Button = ({type,Children, handleClick, className}) => {
-    return(
-        <button type={type} onClick={handleClick} className={className}>
-{Children}
-        </button>
-    )
-}
+const Button = ({ type, Children, handleClick, className }) => {
+  return (
+    <button type={type} onClick={handleClick} className={className}>
+      {Children}
+    </button>
+  );
+};
 
 export default Button;
--- src/components/question.jsx
+++ src/components/question.jsx
@@ -21,79 +21,92 @@ const questiondb = [
     ans: "B",
   },
 ];
-
+// "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
 const Question = () => {
-  // useEffect(
-  //   fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple')
-  //   .then( res => {
-  //     res.JSON()}
+  const [indexNum, setIndexNum] = useState(0);
+  const [questionSource, setQuestionSource] = useState(null);
 
+  // useEffect(() => {
+  //   fetch(
+  //     "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
   //   )
-  //   .then(console.log())
-  // )
-  const [indexNum, setParn] = useState(0);
+  //     .then((res) => res.json())
+  //     .then((dta) => {
+  //       console.log(dta);
+  //       setQuestionSource(dta);
+  //     });
+  // }, []);
+  // chatgpt
+  //  const [quizData, setQuizData] = useState([]);
+
+  // useEffect(() => {
+  //   const fetchQuizData = async () => {
+  //     try {
+  //       const response = await fetch('https://api.example.com/quiz');
+  //       const data = await response.json();
+  //       setQuizData(data);
+  //     } catch (error) {
+  //       console.error('Error fetching quiz data:', error);
+  //     }
+  //   };
+
+  //   fetchQuizData();
+  // }, []);
+  // chat ends here
+  // console.log(questionSource);
+  // console.log(questionSource.results);
+
   // let indexNum = 0;
   function handleNext() {
     // indexNum= indexNum+1
     if (indexNum < 2) {
-      setParn(indexNum + 1);
+      setIndexNum(indexNum + 1);
       console.log(indexNum);
       return indexNum;
     } else alert("quest over bitch!");
   }
   function handePrev() {
     if (indexNum > 0) {
-      setParn(indexNum - 1);
+      setIndexNum(indexNum - 1);
       console.log(indexNum);
     } else alert("what else");
   }
   return (
     <div className="qusestion md:w-[50%] m-auto">
-      <div className="ques p-4 bg-red-500 mb-3">
+      <div className="ques p-4 border-2 border-blue-950 rounded-lg text-2xl mb-3">
         {questiondb[indexNum].ques}
       </div>
-      <div className="btn flex flex-col gap-2">
+      <div className="btn flex flex-col gap-2 mb-2">
         {questiondb[indexNum].opts.map((opt) => (
           <Button
-            className={"bg-green-700 p-4 rounded-lg text-white"}
+            className={"bg-blue-950 p-4 rounded-lg text-white"}
             Children={opt}
             key={opt}
           />
         ))}
-
-        {/* <Button
-          className={"bg-green-700 p-4 rounded-lg"}
-          Children={"Option 2"}
-        />
-        <Button
-          className={"bg-green-700 p-4 rounded-lg"}
-          Children={"Option 3"}
-        />
-        <Button
-          className={"bg-green-700 p-4 rounded-lg mb-2"}
-          Children={"Option 4"}
-        /> */}
       </div>
-      {indexNum <= 2 && (
-        <Button
-          className={"py-2 px-4 bg-blue-950 text-white mr-2 "}
-          Children={"prev"}
-          handleClick={handePrev}
-        />
-      )}
-      {indexNum == 2 && (
-        <Button
-          className={"py-2 px-4 bg-blue-950 text-white mr-2 "}
-          Children={"Submit"}
-          handleClick={handePrev}
-        />
-      )}
+      <div className="next-prev flex justify-between mt-2 pt-2">
+        {indexNum <= 2 && (
+          <Button
+            className={"py-2 px-4 bg-blue-950 text-white mr-2 "}
+            Children={"< prev"}
+            handleClick={handePrev}
+          />
+        )}
+        {indexNum == 2 && (
+          <Button
+            className={"py-2 px-4 bg-blue-950 text-white mr-2 "}
+            Children={"Submit"}
+            handleClick={handePrev}
+          />
+        )}
 
-      <Button
-        className={"py-2 px-4 bg-blue-950 text-white"}
-        Children={"next"}
-        handleClick={handleNext}
-      />
+        <Button
+          className={"py-2 px-4 bg-blue-950 text-white"}
+          Children={"next >"}
+          handleClick={handleNext}
+        />
+      </div>
     </div>
   );
 };
